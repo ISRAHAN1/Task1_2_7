24a025e0a9b11c64de863162e674ac2a90ad72e4
Akobirhon <muhamedzoda_18@mail.ru>
2023-11-11 14:12:54 +0300

    im change veriables.

---
+++ src/Pricee.java
@@ -0,0 +1,16 @@
+public class Pricee {
+    public static void main(String[] args) {
+
+        double price = 500;
+        int count = 1234;
+        double rezult = priceCalculation(price, count);
+
+        System.out.println("Стоимость-" + rezult);
+    }
+        public static double priceCalculation(double price, int count) {
+
+            return price * count;
+
+
+    }
+}
