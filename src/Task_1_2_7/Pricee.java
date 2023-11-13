e3b32e3a879e1a4a82e29b318deda8a79ae8f806
Akobirhon <149703238+ISRAHAN1@users.noreply.github.com>
2023-11-13 15:54:29 +0300

    im change veriables.

--- src/Task_1_2_7/Pricee.java
+++ src/Task_1_2_7/Pricee.java
@@ -3,8 +3,8 @@ package Task_1_2_7;
 public class Pricee {
     public static void main(String[] args) {
 
-        double price = 500;
-        int count = 1234;
+        double price = 40;
+        int count = 4;
         double rezult = priceCalculation(price, count);
 
         System.out.println("Стоимость-" + rezult);
--- src/Task_1_2_8/A12.java
+++ src/Task_1_2_8/A12.java
@@ -1,4 +1,21 @@
 package Task_1_2_8;
-
+import java.lang.Math;
 public class A12 {
-}
+            public static  void main(String[] args) {
+                double a = 5.3;
+                double b = 4.5;
+                double c =a+b;
+
+
+                double rezult = doubleExpression(a, b, c);
+                System.out.println("rez-" + rezult);
+            }
+
+            public static double  doubleExpression(double a, double b, double c) {
+                return c;
+            }
+
+        }
+
+
+
