2a2747b799cba0eeff47cd70dd59657b5c759dfb
Akobirhon <149703238+ISRAHAN1@users.noreply.github.com>
2023-11-15 16:50:25 +0300

    im change veriables.

--- src/Task_1_2_10/A12.java
+++ src/Task_1_2_10/A12.java
@@ -2,16 +2,15 @@ package Task_1_2_10;
 public class A12 {
     public static void main(String[] args) {
         double a=-0.01;
-        double b=0.00;
-        double c=0.01;
-
-
+        double b=-0.00;
+        double c=-0.01;
+        boolean rezult=doubleExpression(a,b,c);
+        System.out.println("rez:"+rezult);
 
         }
 
-    private static double doubleExpression(double a, double b, double c) {
-
-        return a;
+    private static boolean doubleExpression(double a, double b, double c) {
+        return a+b==c||a+b!=c;
     }
 
 
--- src/Task_1_3_2/Bukva.java
+++ src/Task_1_3_2/Bukva.java
@@ -2,7 +2,7 @@ package Task_1_3_2;
 
 public class Bukva{
     public static void main(String[]args){
-        int  a=12;
+        char  a=15;
         double rezult=CharExppression(a);
         System.out.println("rez:"+rezult);
 
@@ -10,6 +10,6 @@ public class Bukva{
 
     }
     public static char CharExppression(int a ){
-        return 1;
+        return  1;
     }
 }
