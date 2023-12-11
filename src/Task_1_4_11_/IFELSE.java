8c9a8a2f6f318440b2ae8563761d3eaf9edf0bd4
Akobirhon <149703238+ISRAHAN1@users.noreply.github.com>
2023-12-11 16:35:02 +0300

    im change veriables.

--- src/Task_1_4_11_/IFELSE.java
+++ src/Task_1_4_11_/IFELSE.java
@@ -3,14 +3,21 @@ import java.util.Scanner;
 public class IFELSE {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Вводите день недели-");
+        System.out.println("Input day -");
         String weekday = in.nextLine();
         System.out.println(isWeekend((weekday)));
     }
 
     public static boolean isWeekend(String weekday) {
-
-        return true;
+        boolean result;
+        if(weekday.equals("Sunday")){
+            result = true;
+        }else if (weekday.equals("Saturday")){
+            result=true;
+        }else{
+            result=false;
+        }
+        return result;
     }
 
 }
