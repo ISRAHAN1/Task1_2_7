e2b6076ae01e142f06a9b863cab9a5d5f36178cb
Akobirhon <149703238+ISRAHAN1@users.noreply.github.com>
2023-12-18 15:34:55 +0300

    im change veriables.

--- src/Task_1_5_3/Massive.java
+++ src/Task_1_5_3/Massive.java
@@ -2,18 +2,16 @@ package Task_1_5_3;
 public class Massive {
     public static void main(String[] args) {
         int[] numbers ={1, 2, 3, 4, 5, 6, 7, 7};
-        printArray(numbers);
+         printArray(numbers);
+
         }
 
 
     public static void printArray(int[] numbers) {
-        int result=0;
-        int[] ds=new int[result];
-        for (int number : numbers) {
-            System.out.println(number);
-
-
-
+        System.out.print(        "["         );
+        for (int i=0;i<numbers.length;i++ ) {
+            System.out.print(","+numbers[i]);
         }
+        System.out.print(        "]"         );
     }
 }
