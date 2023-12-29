716ea87735a76389a24dab9375c8b69176f0473e
Akobirhon <149703238+ISRAHAN1@users.noreply.github.com>
2023-12-29 10:39:36 +0300

    im change veriables.

--- src/Task_1_6_10_/Dif.java
+++ src/Task_1_6_10_/Dif.java
@@ -1,38 +1,10 @@
 package Task_1_6_10_;
+
+import javax.print.attribute.standard.PresentationDirection;
+
 public class Dif {
     public static void main(String[] args) {
-        int[] firstArray = new int[]{1, 3, 7, 5};
-        int[] secondArray = new int[]{8, 4, 2, 4};
-        int[] a = mergeAndSort(firstArray, secondArray);
-        int res = 0;
-        for (int element : a) {
-            if (res != 0)
-                System.out.print(",");
-            System.out.print(element);
-            res = 1;
-        }
-    }
-    public static int[] mergeAndSort(int[] firstArray, int[] secondArray) {
-        int[] result = new int[firstArray.length + secondArray.length];
-        for (int i = 0; i < firstArray.length; i++) {
-            for (int j = 0; j < secondArray.length; j++) {
-                result[i] = firstArray[i];
-                result[firstArray.length + j] = secondArray[j];
-                }
-                }
-        for (int c=0;c<result.length;c++){
-            for (int k=0;k<result.length-c-1;k++){
-                if(result[k]>result[k+1]){
-                    int n=result[k];
-                    result[k]=result[k+1];
-                    result[k+1]=n;
-                }
-            }
 
-        }
-        return result;
+
     }
 }
-
-
-
