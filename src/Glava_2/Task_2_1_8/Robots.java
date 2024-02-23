deb503f4a7cd96cb0d6de417d4b7d011cb0d1922
User <muhamedzoda_18@mail.ru>
2024-02-23 22:16:10 +0300

    im change veriables.

--- src/Glava_2/Task_2_1_8/Robots.java
+++ src/Glava_2/Task_2_1_8/Robots.java
@@ -3,15 +3,13 @@ package Glava_2.Task_2_1_8;
 import static Glava_2.Task_2_1_8.Robot.moveRobot;
 
 public class Robots {
-    public static void main(String[] args) {
-        Robot robot = new Robot(Direction.UP, 3, 0);
-        moveRobot(robot, 3, 0);
+    public static void main(String[]args){
+        Robot robot=new Robot(Direction.UP,3,0);
+        moveRobot(robot,3,0);
         robot.turnRight();
         robot.stepForward();
         robot.stepForward();
         robot.stepForward();
-
     }
 }
-
 
