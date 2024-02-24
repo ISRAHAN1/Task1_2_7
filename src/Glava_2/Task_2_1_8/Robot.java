e66bd38437b7c2f801344f3609d015d60a93d26c
User <muhamedzoda_18@mail.ru>
2024-02-24 22:41:21 +0300

    im change veriables.

--- src/Glava_2/Task_2_1_8/Robot.java
+++ src/Glava_2/Task_2_1_8/Robot.java
@@ -69,26 +69,36 @@ public class Robot {
         } else {
             System.out.println("Робот не находится на заданных координатах");
         }
-        if (robot.getX() != x) {
-            if (robot.getDirection() != Direction.LEFT) {
-                robot.turnRight();
-            } else {
+        if (x < robot.getX()) {
+            while (robot.getDirection() != Direction.LEFT) {
                 robot.turnLeft();
             }
             while (robot.getX() != x) {
                 robot.stepForward();
             }
-            if (robot.getY() != y) {
-                if (robot.getDirection() != Direction.DOWN) {
-                    robot.turnRight();
-                } else {
-                    robot.turnLeft();
-
-                }
-                while (robot.getY() != y) {
-                    robot.stepForward();
-                }
+        } else {
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
             }
+        }
+        while (robot.getX() != x) {
+            robot.stepForward();
+        }
+        if (robot.getY() < y) {
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnLeft();
+            }
+            while (robot.getY()!=y){
+                robot.stepForward();
+            }
+        } else {
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnRight();
+            }
+        }
+        while (robot.getY() != y) {
+            robot.stepForward();
         }
     }
 }
+
--- src/Glava_2/Task_2_1_8/Robots.java
+++ src/Glava_2/Task_2_1_8/Robots.java
@@ -3,13 +3,8 @@ package Glava_2.Task_2_1_8;
 import static Glava_2.Task_2_1_8.Robot.moveRobot;
 
 public class Robots {
-    public static void main(String[]args){
-        Robot robot=new Robot(Direction.UP,3,0);
-        moveRobot(robot,3,0);
-        robot.turnRight();
-        robot.stepForward();
-        robot.stepForward();
-        robot.stepForward();
+    public static void main(String[] args) {
+        Robot robot = new Robot(Direction.UP, 5, 0);
+        moveRobot(robot, 5, 0);
     }
 }
-
