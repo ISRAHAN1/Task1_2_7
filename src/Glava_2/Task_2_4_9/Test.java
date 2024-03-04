c929a6e608dc6aa14803b12fe608256917416c02
User <muhamedzoda_18@mail.ru>
2024-03-04 13:48:54 +0300

    im change veriables.

--- src/Glava_2/Task_2_4_9/Test.java
+++ src/Glava_2/Task_2_4_9/Test.java
@@ -2,7 +2,8 @@ package Glava_2.Task_2_4_9;
 
 public class Test {
     public static void main(String[]args) {
-        Students.JavaStudent res=new Students.JavaStudent();
+        Students.Student res=new Students.JavaStudent();
         res.study();
+
     }
 }
