0144fc950e2ec468375338bb72c64bc2854a6641
User <muhamedzoda_18@mail.ru>
2024-03-26 22:23:05 +0300

    im change veriables.

--- src/Glava_3/Task_3_1_5/CallerClass.java
+++ src/Glava_3/Task_3_1_5/CallerClass.java
@@ -2,15 +2,26 @@ package Glava_3.Task_3_1_5;
 
 public class CallerClass {
     public static void main(String[]args){
+        System.out.println(getCallerClassAndMethodName());
+        anotherMethod();
 
     }
-    private static String anotherMethod() {
+    private static void anotherMethod() {
         System.out.println(getCallerClassAndMethodName());
-        return null;
     }
 
     public static String getCallerClassAndMethodName() {
-        return anotherMethod();
+        Thread current = Thread.currentThread();
+        StackTraceElement[] traceElements = current.getStackTrace();
+
+        if (traceElements.length < 4) {
+            return null;
+        }
+
+        final StackTraceElement trace = traceElements[3];
+
+        return trace.getClassName() + "#" + trace.getMethodName();
+
 
     }
 }
