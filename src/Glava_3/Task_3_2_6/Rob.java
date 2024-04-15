e622aacd15cee486990dd708cf56fc8c3515214e
User <muhamedzoda_18@mail.ru>
2024-04-15 17:09:23 +0300

    im change veriables.

--- src/Glava_3/Task_3_2_6/Rob.java
+++ src/Glava_3/Task_3_2_6/Rob.java
@@ -1,14 +1,16 @@
 package Glava_3.Task_3_2_6;
 public class Rob {
-    public static void main(String[] args) {
-        Robot robot = new Robot();
-        robot.getConnection();
-        robot.close();
-        robot.moveRobotTo(10,12);
+    public static void main(String[] args) throws RobotConnectionException {
+        Robot robot= new Robot();
+        RobotConnection robotConnection =robot.getConnection();
+        robotConnection.close();
+        robotConnection.moveRobotTo(10, 12);
 
 
+        }
     }
-}
+
+
      class Robot implements RobotConnectionManager, RobotConnection {
         int x;
         int y;
@@ -20,11 +22,11 @@ public class Rob {
         public int getY() {
             return y;
         }
-    public void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
+    public void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) throws  RobotConnectionException{
         robotConnectionManager.getConnection();
     }
     @Override
-    public void moveRobotTo(int x, int y) {
+    public void moveRobotTo(int x, int y) throws  RobotConnectionException {
         System.out.println("Начальное положение x: " + this.getX() + "  y :" + this.getY());
         this.x = x;
         this.y = y;
