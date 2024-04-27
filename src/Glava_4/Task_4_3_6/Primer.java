5273fef9a2694dcba1c31732ef344bad70e64359
User <muhamedzoda_18@mail.ru>
2024-04-27 17:25:55 +0300

    im change veriables.

--- src/Glava_4/Task_4_3_6/Primer.java
+++ src/Glava_4/Task_4_3_6/Primer.java
@@ -1,22 +1,22 @@
 package Glava_4.Task_4_3_6;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.charset.Charset;
-public class Primer {
-    public static void main(String[]args) throws  IOException{
-        byte[] array = new byte[]{48 ,49, 50 ,51};
-        InputStream inputStream =  new ByteArrayInputStream(array);
+import java.nio.charset.StandardCharsets;
 
-    }
+
+public class Primer {
+    public static void main(String[]args) throws  IOException {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(new byte[]{48, 49, 50, 51});
+        String res = readAsString(inputStream, Charset.forName("ASCII"));
+        System.out.println(res);
+        }
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
         InputStreamReader reader = new InputStreamReader(inputStream, charset);
-        StringBuilder bio = new StringBuilder();
+        StringWriter bio = new StringWriter();
         int a;
         String res;
         while ((a=reader.read())!=-1){
-            bio.append(a);
+            bio.write(a);
         }
         res=bio.toString();
         return res;
