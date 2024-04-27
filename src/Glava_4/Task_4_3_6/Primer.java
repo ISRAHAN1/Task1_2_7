95d2256cd467d473dc9253cfe1fca1160e51e37d
User <muhamedzoda_18@mail.ru>
2024-04-27 15:26:03 +0300

    im change veriables.

--- src/Glava_4/Task_4_3_6/Primer.java
+++ src/Glava_4/Task_4_3_6/Primer.java
@@ -1,8 +1,24 @@
 package Glava_4.Task_4_3_6;
-
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 public class Primer {
-    public static void main(String[]args){
+    public static void main(String[]args) throws  IOException{
+        byte[] array = new byte[]{48 ,49, 50 ,51};
+        InputStream inputStream =  new ByteArrayInputStream(array);
 
     }
-
+    public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
+        InputStreamReader reader = new InputStreamReader(inputStream, charset);
+        StringBuilder bio = new StringBuilder();
+        int a;
+        String res;
+        while ((a=reader.read())!=-1){
+            bio.append(a);
+        }
+        res=bio.toString();
+        return res;
+    }
 }
--- src/Glava_4/Task_4_3_7/Primer2.java
+++ src/Glava_4/Task_4_3_7/Primer2.java
@@ -1,18 +1,10 @@
 package Glava_4.Task_4_3_7;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
+
+import java.util.Scanner;
 
 public class Primer2 {
-    public static void main(String[]args) throws IOException {
-        String text="Ы";
-        byte[] r = text.getBytes();
-        ByteArrayInputStream boi = new ByteArrayInputStream(r);
-        int a;
-        while ((a=boi.read())!=-1){
-            System.out.println(a);
-
-        }
+    public static void main(String[] args) {
 
     }
 }
