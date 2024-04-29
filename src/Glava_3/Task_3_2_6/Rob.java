d8edc52da0ff6821419e51aff6c960a14cd44f01
User <muhamedzoda_18@mail.ru>
2024-04-29 17:46:52 +0300

    im change veriables.

--- src/Glava_3/Task_3_2_6/Rob.java
+++ src/Glava_3/Task_3_2_6/Rob.java
@@ -1,30 +1,55 @@
 package Glava_3.Task_3_2_6;
+
 public class Rob {
     public static void main(String[] args) throws RobotConnectionException {
-        Robot robot= new Robot();
-        RobotConnection robotConnection =robot.getConnection();
-        robotConnection.close();
-        robotConnection.moveRobotTo(10, 12);
-        }
+
+
+
+    }
+}
+
+
+class Robot implements RobotConnectionManager, RobotConnection {
+    int x;
+    int y;
+
+    public int getX() {
+        return x;
     }
 
-
-     class Robot implements RobotConnectionManager, RobotConnection {
-        int x;
-        int y;
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
-    public void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) throws  RobotConnectionException{
-        robotConnectionManager.getConnection();
+    public int getY() {
+        return y;
     }
+
+    public void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) throws RobotConnectionException {
+        int count = 0;
+        boolean isRun = false;
+        while ((count < 3)) {
+            try (RobotConnection con = robotConnectionManager.getConnection()) {
+                con.moveRobotTo(toX, toY);
+                isRun = true;
+                count = 3;
+            } catch (RobotConnectionException e) {
+                if (isRun) {
+                    count = 3;
+                } else {
+                    count++;
+                    if (count == 3) {
+                        throw new RobotConnectionException("3", e);
+                    }
+                }
+            } catch (Throwable e) {
+                throw e;
+
+            }
+
+
+        }
+
+    }
+
     @Override
-    public void moveRobotTo(int x, int y) throws  RobotConnectionException {
+    public void moveRobotTo(int x, int y) throws RobotConnectionException {
         System.out.println("Начальное положение x: " + this.getX() + "  y :" + this.getY());
         this.x = x;
         this.y = y;
--- src/Glava_4/Task_4_3_7/Primer2.java
+++ src/Glava_4/Task_4_3_7/Primer2.java
@@ -1,10 +1,25 @@
 package Glava_4.Task_4_3_7;
 
-
+import java.io.IOException;
 import java.util.Scanner;
-
 public class Primer2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+       Scanner scanner = new Scanner(System.in);
+       String[] array= scanner.nextLine().split("");
+       double sum=0;
 
+       for(int i=0; i<array.length; i++){
+           double res=0;
+           try {
+               res=Double.parseDouble(array[i]);
+           }catch (RuntimeException ignore){
+
+           }
+           if(res!=0)
+               sum=sum+res;
+       }
+       System.out.println("Output sum:" +String.format("%.6f",sum));
     }
+
 }
+
