bf075405ee83f71a4f421ff234dc5cdf39e4bb18
User <muhamedzoda_18@mail.ru>
2024-07-20 12:08:30 +0300

    im change veriables.

--- src/Task_5_1_11/GeNer.java
+++ src/Task_5_1_11/GeNer.java
@@ -2,13 +2,8 @@ package Task_5_1_11;
 
 public class GeNer {
     public static void main(String[]args){
-        Box<Integer>box=new Box<Integer>(23);
     }
     static class Box<T>{
         private T object;
-        Box(T object){
-            this.object=object;
-        }
-
     }
 }
